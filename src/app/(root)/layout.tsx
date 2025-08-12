c3fe0053ee08735a73683d9958ecb5c2dcb596a9
VictorPotenciano <victor.potenciano@live.u-tad.com>
2025-08-13 00:17:58 +0200

    se cambia el favicon.ico

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -9,9 +9,6 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Bar App",
   description: "Bar App",
-  icons: {
-    icon: "/favicon.ico", 
-  },
 };
 
 export default function RootLayout({
